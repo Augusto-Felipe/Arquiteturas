a6ff6a38fa7d6ac0f5fe3dc1f13780100402027a
Felipe Augusto <augusto_felipe@icloud.com>
2023-07-19 19:37:25 -0300

    [UPDATE] controllers created

---
+++ Arquiteturas/Arquiteturas/Login/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Arquiteturas
+//
+//  Created by Felipe Augusto Correia on 19/07/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func registerButtonTapped(_ sender: UIButton) {
+        
+    }
+}
---
+++ Arquiteturas/Arquiteturas/Register/RegisterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RegisterViewController.swift
+//  Arquiteturas
+//
+//  Created by Felipe Augusto Correia on 19/07/23.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var confirmPasswordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func registerButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+}
