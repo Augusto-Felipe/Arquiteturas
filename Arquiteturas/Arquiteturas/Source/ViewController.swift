cff347cc9d084ac8808f113aefbf91a92777af94
Felipe Augusto <augusto_felipe@icloud.com>
2023-07-19 18:05:12 -0300

    [UPDATE] controllers removed

---
+++ Arquiteturas/Arquiteturas/Source/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Arquiteturas
+//
+//  Created by Felipe Augusto Correia on 19/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
