cd5902819b49bef8acf42c1ad6c645683259a283
Felipe Augusto <augusto_felipe@icloud.com>
2023-07-21 22:59:14 -0300

    [UPDATE]

---
+++ Arquiteturas/Arquiteturas/Controllers/Home/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  Arquiteturas
+//
+//  Created by Felipe Augusto Correia on 21/07/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    private lazy var screen: HomeScreen = {
+        let screen = HomeScreen()
+        screen.translatesAutoresizingMaskIntoConstraints = false
+        return screen
+    }()
+    
+    
+    override func loadView() {
+        screen = HomeScreen()
+        self.view = screen
+    }
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
---
+++ Arquiteturas/Arquiteturas/Controllers/Home/Screen/HomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreen.swift
+//  Arquiteturas
+//
+//  Created by Felipe Augusto Correia on 21/07/23.
+//
+
+import UIKit
+
+class HomeScreen: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
